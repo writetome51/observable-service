0167cdf7ed89cb4361ee2e825855761372994bc1
Steve Thompson <writetome51@gmail.com>
2019-01-27 00:55:46 -0700

    minor change

--- dist/index.js
+++ dist/index.js
@@ -17,9 +17,6 @@ var base_class_1 = require("@writetome51/base-class");
 /*************
  This class acts as a middleman between the class that creates an observable and the class
  that subscribes to that observable.  That way those 2 classes don't have to know about each other.
- To use:  create a subclass of this, and pass  _objectWithMethodThatReturnsObservable and the
- nameOfMethodThatReturnsObservable to super() inside the constructor.
- Then use the subclass as an injected service inside the class that subscribes to the observable.
  **************/
 var ObservableService = /** @class */ (function (_super) {
     __extends(ObservableService, _super);
--- lib/index.ts
+++ lib/index.ts
@@ -6,9 +6,6 @@ import { IEmptyable } from 'emptyable/IEmptyable';
 /*************
  This class acts as a middleman between the class that creates an observable and the class
  that subscribes to that observable.  That way those 2 classes don't have to know about each other.
- To use:  create a subclass of this, and pass  _objectWithMethodThatReturnsObservable and the
- nameOfMethodThatReturnsObservable to super() inside the constructor.
- Then use the subclass as an injected service inside the class that subscribes to the observable.
  **************/
 
 export abstract class ObservableService extends BaseClass implements IEmptyable {
